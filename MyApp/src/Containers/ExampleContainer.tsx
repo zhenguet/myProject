ca95ee497325a38f6adbeff5967214d024cf76b0
ChinhHV <chinh.hv.nsmv@gmail.com>
2023-08-02 14:42:42 +0700

    add vision camera

--- MyApp/src/Containers/ExampleContainer.tsx
+++ MyApp/src/Containers/ExampleContainer.tsx
@@ -1,101 +1,75 @@
-import React, { useState, useEffect } from 'react'
-import {
-  View,
-  ActivityIndicator,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  ScrollView,
-} from 'react-native'
-import { useDispatch } from 'react-redux'
-import { useTranslation } from 'react-i18next'
-import { Brand } from '@/Components'
-import { useTheme } from '@/Hooks'
-import { useLazyFetchOneQuery } from '@/Services/modules/users'
-import { changeTheme, ThemeState } from '@/Store/Theme'
+import React, { useRef, useEffect } from 'react'
+import { View, Animated } from 'react-native'
+import { Camera, useCameraDevices } from 'react-native-vision-camera'
+import Svg, { Circle } from 'react-native-svg'
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle)
 
 const ExampleContainer = () => {
-  const { t } = useTranslation()
-  const { Common, Fonts, Gutters, Layout } = useTheme()
-  const dispatch = useDispatch()
-
-  const [userId, setUserId] = useState('9')
-  const [fetchOne, { data, isSuccess, isLoading, isFetching, error }] =
-    useLazyFetchOneQuery()
+  const cameraRef = useRef(null)
+  const animatedValue = useRef(new Animated.Value(0)).current
+  const devices = useCameraDevices()
+  const device = devices.front
 
   useEffect(() => {
-    fetchOne(userId)
-  }, [fetchOne, userId])
+    Camera.requestCameraPermission()
+    startAnimation()
+  }, [])
 
-  const onChangeTheme = ({ theme, darkMode }: Partial<ThemeState>) => {
-    dispatch(changeTheme({ theme, darkMode }))
+  const startAnimation = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(animatedValue, {
+          toValue: 1,
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(animatedValue, {
+          toValue: 0,
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start()
   }
 
+  const circleSize = 100
+  const animatedSize = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [circleSize, circleSize * 2], // Tăng kích thước từ circleSize lên gấp đôi
+  })
+
+  if (device == null) return <View />
   return (
-    <ScrollView
-      style={Layout.fill}
-      contentContainerStyle={[
-        Layout.fill,
-        Layout.colCenter,
-        Gutters.smallHPadding,
-      ]}
-    >
-      <View style={[[Layout.colCenter, Gutters.smallHPadding]]}>
-        <Brand />
-        {(isLoading || isFetching) && <ActivityIndicator />}
-        {!isSuccess ? (
-          <Text style={Fonts.textRegular}>{error}</Text>
-        ) : (
-          <Text style={Fonts.textRegular}>
-            {t('example.helloUser', { name: data?.name })}
-          </Text>
-        )}
-      </View>
-      <View
-        style={[
-          Layout.row,
-          Layout.rowHCenter,
-          Gutters.smallHPadding,
-          Gutters.largeVMargin,
-          Common.backgroundPrimary,
-        ]}
+    <View style={{ flex: 1 }}>
+      <Camera
+        ref={cameraRef}
+        style={{
+          flex: 1,
+          borderRadius: 100,
+        }}
+        device={device}
+        isActive={true}
+      />
+      <Svg
+        height="100%"
+        width="100%"
+        style={{
+          position: 'absolute',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
       >
-        <Text style={[Layout.fill, Fonts.textCenter, Fonts.textSmall]}>
-          {t('example.labels.userId')}
-        </Text>
-        <TextInput
-          onChangeText={setUserId}
-          editable={!isLoading}
-          keyboardType={'number-pad'}
-          maxLength={1}
-          value={userId}
-          selectTextOnFocus
-          style={[Layout.fill, Common.textInput]}
+        <AnimatedCircle
+          cx="50%"
+          cy="50%"
+          r={animatedSize}
+          fill="transparent"
+          stroke="red"
+          strokeWidth="2"
         />
-      </View>
-      <Text style={[Fonts.textRegular, Gutters.smallBMargin]}>DarkMode :</Text>
-
-      <TouchableOpacity
-        style={[Common.button.rounded, Gutters.regularBMargin]}
-        onPress={() => onChangeTheme({ darkMode: null })}
-      >
-        <Text style={Fonts.textRegular}>Auto</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={[Common.button.outlineRounded, Gutters.regularBMargin]}
-        onPress={() => onChangeTheme({ darkMode: true })}
-      >
-        <Text style={Fonts.textRegular}>Dark</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={[Common.button.outline, Gutters.regularBMargin]}
-        onPress={() => onChangeTheme({ darkMode: false })}
-      >
-        <Text style={Fonts.textRegular}>Light</Text>
-      </TouchableOpacity>
-    </ScrollView>
+      </Svg>
+    </View>
   )
 }
 
