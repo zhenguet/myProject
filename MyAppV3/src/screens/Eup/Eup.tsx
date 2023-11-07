364a4a1fab5844e7e451c9cef6174bf330cba423
Zuet <zuet@eup.net.vn>
2023-11-07 16:46:11 +0700

    Update Euo

--- MyAppV3/src/screens/Eup/Eup.tsx
+++ MyAppV3/src/screens/Eup/Eup.tsx
@@ -1,28 +1,22 @@
 import React, { useLayoutEffect, useState } from 'react';
 import {
   Dimensions,
-  Pressable,
   SafeAreaView,
-  ScrollView,
   StyleSheet,
   Text,
   View,
   useColorScheme,
 } from 'react-native';
-import {
-  Gesture,
-  GestureDetector,
-  GestureHandlerRootView,
-} from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Animated, {
-  useAnimatedReaction,
+  Extrapolate,
+  interpolate,
+  useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
-  withSpring,
-  withTiming,
 } from 'react-native-reanimated';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { calPosition, move, setPosition } from './Layout';
+import Item from './Item';
 
 const words = [
   { id: 1, text: 'Thiên' },
@@ -47,7 +41,12 @@ const words = [
   { id: 20, text: 'cẩu' },
 ];
 
+const itemPerPage = 12;
+
+const images = Array.from(Array(Math.ceil(words.length / itemPerPage)).keys());
+
 const { width, height } = Dimensions.get('window');
+const containerWidth = width * 0.75;
 const INNER_MARGIN = 10;
 function Eup(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -58,6 +57,7 @@ function Eup(): JSX.Element {
   };
 
   const ready = useSharedValue(true);
+  const translateX = useSharedValue(0);
 
   // giá trị original để đánh dấu, sẽ chỉ cập nhật sau khi quá trình thả
   const offsets = words.map((item, index) => ({
@@ -75,15 +75,15 @@ function Eup(): JSX.Element {
   useLayoutEffect(() => {
     if (!loading) {
       // vòng lặp theo dòng
-      for (let gIndex = 0; gIndex < offsets.length / 4; gIndex++) {
+      for (let gIndex = 0; gIndex < offsets.length / 3; gIndex++) {
         const first = offsets[gIndex]; // phần tử đầu tiên của dòng
         // set giá trị heiht của dòng bằng nhau, theo phần tử cao nhất
         let maxHeight = first.height.value;
-        for (let i = 1; i < 4; i++) {
-          if (offsets[gIndex * 4 + i]) {
+        for (let i = 1; i < 3; i++) {
+          if (offsets[gIndex * 3 + i]) {
             maxHeight = Math.max(
               first.height.value,
-              offsets[gIndex * 4 + i].height.value,
+              offsets[gIndex * 3 + i].height.value,
             );
           }
         }
@@ -92,25 +92,36 @@ function Eup(): JSX.Element {
         let yValue = 0;
         if (gIndex > 0) {
           for (let i = 0; i < gIndex; i++) {
-            yValue += offsets[i * 4].height.value;
+            yValue += offsets[i * 3].height.value;
           }
         }
 
         // set giá trị từng phần tử
-        for (let i = 0; i < 4; i++) {
-          if (offsets[gIndex * 4 + i]) {
-            const item = offsets[gIndex * 4 + i];
+        for (let i = 0; i < 3; i++) {
+          if (offsets[gIndex * 3 + i]) {
+            const item = offsets[gIndex * 3 + i];
             item.height.value = maxHeight;
             item.x.value = item.width.value * i;
             item.originalX.value = item.width.value * i;
             item.y.value = yValue;
             item.originalY.value = yValue;
-            item.originalOrder.value = gIndex * 4 + i;
+            item.originalOrder.value = gIndex * 3 + i;
           }
         }
       }
     }
   }, [loading]);
+
+  const scrollHandler = useAnimatedScrollHandler(
+    {
+      onScroll: event => {
+        translateX.value = event.contentOffset.x;
+      },
+      onBeginDrag: () => {},
+      onEndDrag: () => {},
+    },
+    [],
+  );
 
   return (
     <SafeAreaView
@@ -124,159 +135,105 @@ function Eup(): JSX.Element {
     >
       <GestureHandlerRootView style={styles.container}>
         <View style={[styles.panContainer, { margin: INNER_MARGIN }]}>
-          <ScrollView style={{ height: 100 }}>
-            <View
-              style={[styles.innerPanContainer]}
-              onLayout={() => setLoading(false)}
+          <View style={styles.scrollContainer}>
+            <Animated.ScrollView
+              onScroll={scrollHandler}
+              pagingEnabled
+              scrollEventThrottle={16}
+              horizontal
+              showsHorizontalScrollIndicator={false}
             >
-              {words.map((item, index) => (
-                <View
-                  style={[styles.itemWrap, { opacity: 0 }]}
-                  onLayout={({ nativeEvent: { layout } }) => {
-                    const offset = offsets[index];
-                    offset.width.value = layout.width + 5;
-                    offset.height.value = layout.height + 5;
-                  }}
-                  key={index}
-                >
-                  <Text style={styles.itemText}>{item.text}</Text>
-                </View>
-              ))}
+              {images.map((image, imageIndex) => {
+                const from = itemPerPage * imageIndex;
+                const to = from + itemPerPage;
+
+                return (
+                  <View style={{ width: containerWidth }} key={imageIndex}>
+                    <View style={styles.card}>
+                      <View
+                        style={[styles.innerPanContainer]}
+                        onLayout={() => setLoading(false)}
+                      >
+                        {words.slice(from, to).map((item, index) => (
+                          <View
+                            style={[styles.itemWrap, { opacity: 0 }]}
+                            onLayout={({ nativeEvent: { layout } }) => {
+                              const offset = offsets[index];
+                              offset.width.value = layout.width + 5;
+                              offset.height.value = layout.height + 5;
+                            }}
+                            key={`${item.id}_${index}`}
+                          >
+                            <Text style={styles.itemText}>{item.text}</Text>
+                          </View>
+                        ))}
+                      </View>
+                      <View style={{ position: 'absolute' }}>
+                        {words.slice(from, to).map((item, index) => (
+                          <Item
+                            item={item}
+                            offsets={offsets}
+                            ready={ready}
+                            index={index}
+                            key={item.id}
+                          />
+                        ))}
+                      </View>
+                    </View>
+                  </View>
+                );
+              })}
+            </Animated.ScrollView>
+            <View style={styles.indicatorContainer}>
+              {images.map((image, imageIndex) => {
+                return (
+                  <Dot
+                    key={`${image}_${imageIndex}`}
+                    translateX={translateX}
+                    inputRange={[
+                      containerWidth * (imageIndex - 1),
+                      containerWidth * imageIndex,
+                      containerWidth * (imageIndex + 1),
+                    ]}
+                  />
+                );
+              })}
             </View>
-            <View style={{ position: 'absolute' }}>
-              {words.map((item, index) => (
-                <Item
-                  item={item}
-                  offsets={offsets}
-                  ready={ready}
-                  index={index}
-                  key={item.id}
-                />
-              ))}
-            </View>
-          </ScrollView>
+          </View>
         </View>
       </GestureHandlerRootView>
     </SafeAreaView>
   );
 }
-const Item = ({ item, ready, offsets, index }: any) => {
-  // phần tử hiện tại
-  const offset = offsets[index];
 
-  // đánh dấu đang thao tác
-  const isSelected = useSharedValue(false);
+const Dot = (props: any) => {
+  const { translateX, inputRange } = props;
 
-  // hiển thị delete button
-  const isShowDelBtn = useSharedValue(false);
-
-  // lưu giá trị ban đầu của item với vị trí trên cùng của list
-  const start = useSharedValue({ x: offset.x.value, y: offset.y.value });
-
-  /** trigger cập nhật giá trị theo `originalOrder` của tất cả các item khi kết thúc */
-  useAnimatedReaction(
-    () => {
-      return offset.originalOrder.value;
-    },
-    (currentValue, previousValue) => {
-      if (currentValue !== previousValue) {
-        start.value = {
-          x: offset.x.value,
-          y: offset.y.value,
-        };
-
-        offset.originalX.value = offset.x.value;
-        offset.originalY.value = offset.y.value;
-        isShowDelBtn.value = false;
-      }
-    },
-  );
-
-  // pan gesture
-  const pan = Gesture.Pan()
-    .onStart(() => {
-      isSelected.value = true;
-      isShowDelBtn.value = true;
-      ready.value = false;
-    })
-    .onUpdate(e => {
-      isShowDelBtn.value = false;
-      // di chuyển item
-      offset.x.value = e.translationX + start.value.x;
-      offset.y.value = e.translationY + start.value.y;
-
-      const newIndex = calPosition(offset, offsets);
-
-      move(offset.originalOrder.value, newIndex, offsets);
-    })
-    .onFinalize(() => {
-      isSelected.value = false;
-      ready.value = true;
-
-      setPosition(offset, offsets);
-    });
-  pan.config = { activateAfterLongPress: 500, minDist: 10 };
-
-  const animatedStyles = useAnimatedStyle(() => {
+  const style = useAnimatedStyle(() => {
     return {
+      width: interpolate(
+        translateX.value,
+        inputRange,
+        [8, 16, 8],
+        Extrapolate.CLAMP,
+      ),
       transform: [
         {
-          translateX: isSelected.value
-            ? offset.x.value
-            : withTiming(offset.x.value, { duration: 200 }),
+          scale: interpolate(
+            translateX.value,
+            inputRange,
+            [1, 1.2, 1],
+            Extrapolate.CLAMP,
+          ),
         },
-        {
-          translateY: isSelected.value
-            ? offset.y.value
-            : withTiming(offset.y.value, { duration: 200 }),
-        },
-        { scale: withSpring(isSelected.value ? 1.2 : 1) },
       ],
-      backgroundColor: ready.value
-        ? 'red'
-        : isSelected.value
-        ? 'gray'
-        : 'white',
-      zIndex: isSelected.value ? 100000 : 1,
     };
   });
 
-  const animatedText = useAnimatedStyle(() => {
-    return {
-      color: !ready.value && !isSelected.value ? 'gray' : 'white',
-    };
-  });
-
-  const animatedDelBtn = useAnimatedStyle(() => ({
-    display: isShowDelBtn.value ? 'flex' : 'none',
-  }));
-  return (
-    <GestureDetector gesture={pan}>
-      <Animated.View
-        style={[styles.itemWrap, { position: 'absolute' }, animatedStyles]}
-      >
-        <Animated.Text style={[styles.itemText, animatedText]}>
-          {item.text}
-        </Animated.Text>
-        <Pressable style={{ position: 'absolute' }}>
-          <Animated.View
-            style={[
-              {
-                width: 10,
-                height: 10,
-                backgroundColor: 'white',
-                borderRadius: 10,
-              },
-              animatedDelBtn,
-            ]}
-          />
-        </Pressable>
-      </Animated.View>
-    </GestureDetector>
-  );
+  return <Animated.View style={[styles.normalDot, style]} />;
 };
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
@@ -302,7 +259,7 @@ const styles = StyleSheet.create({
   itemWrap: {
     backgroundColor: 'red',
     padding: 5,
-    width: width / 6 - 10,
+    width: containerWidth / 4 - INNER_MARGIN,
     borderWidth: 1,
   },
   itemText: {
@@ -326,7 +283,7 @@ const styles = StyleSheet.create({
     // opacity: 0.3,
   },
   panContainer: {
-    width: (width * 2) / 3,
+    width: containerWidth,
     backgroundColor: 'white',
   },
   innerPanContainer: {
@@ -334,6 +291,42 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     gap: 5,
     alignItems: 'center',
+  },
+  scrollContainer: {
+    height: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    flex: 1,
+    marginVertical: 4,
+    marginHorizontal: 16,
+    borderRadius: 5,
+    overflow: 'hidden',
+    backgroundColor: 'aqua',
+  },
+  textContainer: {
+    backgroundColor: 'rgba(0,0,0, 0.7)',
+    paddingHorizontal: 24,
+    paddingVertical: 8,
+    borderRadius: 5,
+  },
+  infoText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  indicatorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  normalDot: {
+    height: 8,
+    width: 8,
+    borderRadius: 4,
+    backgroundColor: 'silver',
+    marginHorizontal: 4,
   },
 });
 
---
+++ MyAppV3/src/screens/Eup/Item.tsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import { Pressable } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedReaction,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
+import { styles } from './Eup';
+import { calPosition, move, setPosition } from './Layout';
+
+const Item = ({ item, ready, offsets, index }: any) => {
+  // phần tử hiện tại
+  const offset = offsets[index];
+
+  // đánh dấu đang thao tác
+  const isSelected = useSharedValue(false);
+
+  // hiển thị delete button
+  const isShowDelBtn = useSharedValue(false);
+
+  // lưu giá trị ban đầu của item với vị trí trên cùng của list
+  const start = useSharedValue({ x: offset.x.value, y: offset.y.value });
+
+  /** trigger cập nhật giá trị theo `originalOrder` của tất cả các item khi kết thúc */
+  useAnimatedReaction(
+    () => {
+      return offset.originalOrder.value;
+    },
+    (currentValue, previousValue) => {
+      if (currentValue !== previousValue) {
+        start.value = {
+          x: offset.x.value,
+          y: offset.y.value,
+        };
+
+        offset.originalX.value = offset.x.value;
+        offset.originalY.value = offset.y.value;
+        isShowDelBtn.value = false;
+      }
+    },
+  );
+
+  // pan gesture
+  const pan = Gesture.Pan()
+    .onStart(() => {
+      isSelected.value = true;
+      isShowDelBtn.value = true;
+      ready.value = false;
+    })
+    .onUpdate(e => {
+      isShowDelBtn.value = false;
+      // di chuyển item
+      offset.x.value = e.translationX + start.value.x;
+      offset.y.value = e.translationY + start.value.y;
+
+      const newIndex = calPosition(offset, offsets);
+
+      move(offset.originalOrder.value, newIndex, offsets);
+    })
+    .onFinalize(() => {
+      isSelected.value = false;
+      ready.value = true;
+
+      setPosition(offset, offsets);
+    });
+  pan.config = { activateAfterLongPress: 500, minDist: 10 };
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: isSelected.value
+            ? offset.x.value
+            : withTiming(offset.x.value, { duration: 200 }),
+        },
+        {
+          translateY: isSelected.value
+            ? offset.y.value
+            : withTiming(offset.y.value, { duration: 200 }),
+        },
+        { scale: withSpring(isSelected.value ? 1.2 : 1) },
+      ],
+      backgroundColor: ready.value
+        ? 'red'
+        : isSelected.value
+        ? 'gray'
+        : 'white',
+      zIndex: isSelected.value ? 100000 : 1,
+    };
+  });
+
+  const animatedText = useAnimatedStyle(() => {
+    return {
+      color: !ready.value && !isSelected.value ? 'gray' : 'white',
+    };
+  });
+
+  const animatedDelBtn = useAnimatedStyle(() => ({
+    display: isShowDelBtn.value ? 'flex' : 'none',
+  }));
+  return (
+    <GestureDetector gesture={pan}>
+      <Animated.View
+        style={[styles.itemWrap, { position: 'absolute' }, animatedStyles]}
+      >
+        <Animated.Text style={[styles.itemText, animatedText]}>
+          {item.text}
+        </Animated.Text>
+        <Pressable style={{ position: 'absolute' }}>
+          <Animated.View
+            style={[
+              {
+                width: 10,
+                height: 10,
+                backgroundColor: 'white',
+                borderRadius: 10,
+              },
+              animatedDelBtn,
+            ]}
+          />
+        </Pressable>
+      </Animated.View>
+    </GestureDetector>
+  );
+};
+
+export default Item;
